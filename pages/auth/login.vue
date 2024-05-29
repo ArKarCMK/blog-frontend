9781b40f9dcfb1c8e66d57a4f1bfc5fa4dd6fb9e
Arkar <arkarchanmyaeko@gmail.com>
2024-05-29 16:04:42 +0700

    add: add login and register page ui

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,15 +1,65 @@
 <template>
-  <n-space>
-    <n-button>Default</n-button>
-    <n-button type="tertiary"> Tertiary </n-button>
-    <n-button type="primary"> Primary </n-button>
-    <n-button type="info"> Info </n-button>
-    <n-button type="success"> Success </n-button>
-    <n-button type="warning"> Warning </n-button>
-    <n-button type="error"> Error </n-button>
-  </n-space>
+  <div class="container">
+    <div class="sub-container">
+      <div class="login">Login</div>
+      <div class="email">Email</div>
+      <n-input v-model:value="email" type="text" placeholder="Email" />
+      <div class="password">Password</div>
+      <n-input v-model:value="value" type="text" placeholder="Password" />
+      <div class="wrap-button">
+        <n-button @click="handleClick" class="loginBtn" type="primary" tertiary>
+          Login
+        </n-button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const email = ref("");
+const password = ref("");
 
-<style></style>
+const handleClick = () => {
+  console.log("login.value", email);
+};
+</script>
+
+<style lang="scss">
+.container {
+  width: 100%;
+  height: 100vh;
+  // background: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .sub-container {
+    width: 500px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .login {
+      text-align: center;
+      font-size: 20px;
+      font-weight: bold;
+    }
+    .email {
+      padding-bottom: 10px;
+    }
+    .password {
+      padding: 10px 0;
+    }
+    .wrap-button {
+      padding-top: 10px;
+      // background: green;
+      display: flex;
+      justify-content: center;
+      .loginBtn {
+        width: 200px;
+        background: green;
+        border-radius: 5px;
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,9 +1,77 @@
 <template>
-  <div>Hello, I'm register.</div>
+  <div class="container">
+    <div class="sub-container">
+      <div class="login">Register</div>
+
+      <div class="title">Name</div>
+      <n-input v-model:value="name" type="text" placeholder="Name" />
+
+      <div class="title">Email</div>
+      <n-input v-model:value="email" type="text" placeholder="Email" />
+
+      <div class="title">Password</div>
+      <n-input v-model:value="value" type="text" placeholder="Password" />
+
+      <div class="title">Please Enter Your Password Again</div>
+      <n-input v-model:value="value" type="text" placeholder="Password" />
+
+      <div class="wrap-button">
+        <n-button
+          @click="handleClick"
+          class="registerBtn"
+          type="primary"
+          tertiary
+        >
+          Register
+        </n-button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+const email = ref("");
+const password = ref("");
+
+const handleClick = () => {
+  console.log("login.value", email);
+};
 </script>
 
-<style></style>
+<style lang="scss">
+.container {
+  width: 100%;
+  height: 100vh;
+  // background: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .sub-container {
+    width: 500px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .login {
+      text-align: center;
+      font-size: 20px;
+      font-weight: bold;
+    }
+    .title {
+      padding: 10px 0;
+    }
+    .wrap-button {
+      padding-top: 10px;
+      // background: green;
+      display: flex;
+      justify-content: center;
+      .registerBtn {
+        margin-top: 20px;
+        width: 200px;
+        background: green;
+        border-radius: 5px;
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
